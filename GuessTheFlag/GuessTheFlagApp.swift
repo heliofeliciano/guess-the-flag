da74d2f4603342b0f53a96729b620dc884a7609b
Helio Feliciano <heliofeliciano@gmail.com>
2023-02-18 19:02:48 -0300

    first commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Helio Feliciano on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
